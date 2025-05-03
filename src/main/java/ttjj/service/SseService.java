0c123d37f919402a4b897c98975e54d8b606a2cb
czl315 <czl315@163.com>
2025-05-03 22:36:10 +0800

    代码优化

--- src/main/java/ttjj/service/SseService.java
+++ src/main/java/ttjj/service/SseService.java
@@ -37,16 +37,16 @@ public class SseService {
     public static void main(String[] args) {
 
         //查询K线-查询交易日列表，查询贵州茅台的日k线
-        int count = 60;
+        int count = 40;
         String endDate = DateUtil.getToday(YYYY_MM_DD);
 //        String stCode = "517400";
-        String zqdm = "159509";
+        String zqdm = "510100";// 159509
 //        String klineRs = SseService.daykRsStrHttp(stCode, 2);
 //        String klineRs = SseService.daykRsStrHttpSz(stCode, 2, CYCLE_TYPE_WEEK);
 //        System.out.println("k线：" + klineRs);
 //        List<Kline> klineList = daykline(stCode, 5);
 //        List<Kline> klineList = dayklineSz(stCode, 61, CYCLE_TYPE_WEEK);
-        List<Kline> klineList = klineExchange(zqdm, count, CYCLE_TYPE_WEEK);
+        List<Kline> klineList = klineExchange(zqdm, count, CYCLE_TYPE_DAY);//CYCLE_TYPE_WEEK
         int i = 1;
         for (Kline kline : klineList) {
             System.out.println((i++) + ":" + kline.getZqdm() + ":" + kline.getKtime() + ",今日收盘：" + kline.getCloseAmt() + ",涨幅：" + kline.getZhangDieFu());
