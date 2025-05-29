d19ecaa408c00fb56c135aa3c6c066bac7a9b2fc
czl315 <czl315@163.com>
2025-05-30 00:23:21 +0800

    代码优化

--- src/main/java/ttjj/rank/EtfControl.java
+++ src/main/java/ttjj/rank/EtfControl.java
@@ -53,14 +53,14 @@ public class EtfControl {
         condition.setMaKltList(Arrays.asList(KLT_15, KLT_30, KLT_60, KLT_101, KLT_102));//价格区间周期列表
 //        condition.setMaKltList(Arrays.asList(KLT_102));//价格区间周期列表
 
-//        saveOrUpdateListNetLastDay(condition, date);//保存或更新ETF涨幅次数-批量更新基础信息
-//        List<RankBizDataDiff> etfList = listEtfListLastDayByMarketValue(null, null, null);//1、查询etf列表   JINRONG_GOLD
-//        updateAdrSumSse(date, etfList);
-//        updateUpSumOrder(date);
-//        updateLatestDayAdr(condition, date, httpKlineApiType);
+        saveOrUpdateListNetLastDay(condition, date);//保存或更新ETF涨幅次数-批量更新基础信息
+        List<RankBizDataDiff> etfList = listEtfListLastDayByMarketValue(null, null, null);//1、查询etf列表   JINRONG_GOLD
+        updateAdrSumSse(date, etfList);
+        updateUpSumOrder(date);
+        updateLatestDayAdr(condition, date, httpKlineApiType);
 //        List<EtfAdrCountVo> stockAdrCountList = EtfAdrCountService.findEtfList(condition);//查询列表-根据条件
 //        updateUpMaExchange(date, stockAdrCountList, condition, API_TYPE_SSE);//更新-超过均线信息（交易所）
-        updateUpMaTypeTopN(date, 2,Arrays.asList(KLT_15, KLT_30, KLT_60, KLT_101, KLT_102));//更新超过均线-每个类型涨幅前n个  Waing：数量过多超过东财访问次数限定
+//        updateUpMaTypeTopN(date, 2,Arrays.asList(KLT_15, KLT_30, KLT_60, KLT_101, KLT_102));//更新超过均线-每个类型涨幅前n个  Waing：数量过多超过东财访问次数限定
 //        updateNetArea(date, stockAdrCountList, httpKlineApiType);//更新-价格区间
 
 //        findByDateOrder(date, zqdmList, 100,NET_AREA_DAY_20 , 200, null,2);//查询数据根据日期，按照涨幅倒序    F3_DESC  NET_AREA_DAY_20
